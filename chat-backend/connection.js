770fadebebb990f2b4e7c4de38283323e5af9305
filipgerdina <99407737+filipgerdina@users.noreply.github.com>
2022-12-23 23:09:06 +0100

    Update connection.js

--- chat-backend/connection.js
+++ chat-backend/connection.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+ require('dotenv').config();
 
-mongoose.connect(
-	process.env.DB,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	},
-	() => {
-		console.log('connected to database myDb ;)');
-	}
-);
+ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.s4yoqpu.mongodb.net/?retryWrites=true&w=majority`, ()=> {
+   console.log('connected to mongodb')
+ })
